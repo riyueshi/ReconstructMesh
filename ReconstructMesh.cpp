71799486713e30fed5fb27785bd25164f361701c
riyueshi <imriyueshi@163.com>
2016-05-05 14:51:21 +0800

    pMat2xml
    projection Matrix file convert to photoScan xml format file

--- ReconstructMesh.cpp
+++ ReconstructMesh.cpp
@@ -30,33 +30,22 @@
 */
 
 #include <string>
+#include <fstream>
+#include <iostream>
+#include <iomanip>
 
 #include "../../libs/MVS/Common.h"
 #include "../../libs/MVS/Scene.h"
 #include <boost/program_options.hpp>
 
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/random_simplify_point_set.h>
-#include <CGAL/grid_simplify_point_set.h>
-#include <CGAL/IO/read_xyz_points.h>
-#include <boost/property_map/property_map.hpp>
-#include <CGAL/IO/write_xyz_points.h>
-
-#include <opencv2/core/core.hpp>
-#include <opencv2/core/utility.hpp>
-#include <opencv2/highgui/highgui.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
-#include <opencv/cv.h>
-
 //#include "Camera.h"
 
 using namespace MVS;
 using namespace std;
+typedef long double Longouble;
+typedef TMatrix<double, 3, 4> Matrix3x4ld;
 
-// D E F I N E S ///////////////////////////////////////////////////
-
-#define APPNAME _T("ReconstructMesh")
-
+#define APPNAME _T("ProjectMatrix2PhotoScanXML")
 
 bool get_filelist_from_dir(string path, vector<string>& files)
 {
@@ -77,204 +66,111 @@ bool get_filelist_from_dir(string path, vector<string>& files)
 		return false;
 }
 
-
-bool find_in_map(cv::Mat &lineMap, const double &x, const double &y)
+bool readInPmatFile(vector<string> &pMatNameVec, vector<Matrix3x4d> &pMatVec)
 {
-	if (int(x)<0 || int(x)>lineMap.cols - 1 || int(y)<0 || int(y)>lineMap.rows - 1)
+	for (int i = 0; i < pMatNameVec.size(); i++)
 	{
-		return false;
-	}
-	return (lineMap.at<uchar>(int(y), int(x)) == 255 ? true : false);
-}
-
-bool construct_line_map(Scene &scene, string undistortImagePath, vector<cv::Mat> &lineMapVec, int lineWidth)
-{
-	const int maxDis(lineWidth);
-	string imageSearchPath = undistortImagePath + "\\*.tif";
-	vector<string> imageNameVec;
-	get_filelist_from_dir(imageSearchPath, imageNameVec);
-	for (int imageIndex = 0; imageIndex < imageNameVec.size(); imageIndex++)
-	{
-		cout << "processing image " << imageIndex << endl;
-		using namespace cv;
-		vector<Point> pointOnLine;
-
-		Mat image = cv::imread(undistortImagePath + "\\" + imageNameVec.at(imageIndex), IMREAD_COLOR);
-		if (!image.data)
+		ifstream pMatReader(pMatNameVec.at(i));
+		if (!pMatReader)
 		{
-			cout << "can not load image " << undistortImagePath + "\\" + imageNameVec.at(imageIndex) << endl;
-			waitKey(3000);
-			return false;
+			cout << "open projection matrix " << pMatNameVec.at(i) << " failed!" << endl;
+			getchar();
 		}
-
-		Mat imageCanny;
-		cv::cvtColor(image, imageCanny, CV_BGR2GRAY);
-		Canny(imageCanny, imageCanny, 170, 230, 3, true); // Apply canny edge
-
-		vector <Mat> bgr_chanels;
-		split(image, bgr_chanels);
-
-
-
-		for (int chanelIndex = 0; chanelIndex < 3; chanelIndex++)
-		{
-			Ptr<LineSegmentDetector> ls = createLineSegmentDetector(LSD_REFINE_STD);
-			vector<cv::Vec4f> lines_std;
-
-			cout << "processing channle " << chanelIndex << endl;
-			// Detect the lines
-			ls->detect(bgr_chanels.at(chanelIndex), lines_std);
-
-			cout << "finshed" << endl;
-			// Show found lines
-			//Mat drawnLines(image.size(), CV_8UC1, Scalar(0));;
-			for (int i = 0; i < lines_std.size(); i++)
-			{
-				double lineLenth2 = (lines_std.at(i)[0] - lines_std.at(i)[2])*(lines_std.at(i)[0] - lines_std.at(i)[2]) + (lines_std.at(i)[1] - lines_std.at(i)[3])*(lines_std.at(i)[1] - lines_std.at(i)[3]);
-				if (lineLenth2<400)
-				{
-					continue;
-				}
-				line(imageCanny, Point(lines_std.at(i)[0], lines_std.at(i)[1]), Point(lines_std.at(i)[2], lines_std.at(i)[3]), 255, maxDis);
-			}
-
-		}
-
-		lineMapVec.push_back(imageCanny);
-		imwrite(string("lsdimage\\") + imageNameVec.at(imageIndex), imageCanny);
+		Matrix3x4d pMat;
+		string tempString;
+		pMatReader >> tempString;
+		pMatReader >> pMat(0, 0) >> pMat(0, 1) >> pMat(0, 2) >> pMat(0, 3)
+					>> pMat(1, 0) >> pMat(1, 1) >> pMat(1, 2) >> pMat(1, 3)
+					>> pMat(2, 0) >> pMat(2, 1) >> pMat(2, 2) >> pMat(2, 3);
+		pMatVec.push_back(pMat);
 	}
+
 	return true;
-
 }
 
-bool pointcloud_simplicate(Scene &scene, vector<Matrix3x4d> &pMatVec,const char*imagePath, double removed_percentage, int lineWidth)
+bool writeXML(vector<Camera> cameraVec, string XMLName, vector<string>imageNameVec)
 {
-	typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
-	typedef Kernel::Point_3 Point;
-	typedef Kernel::Vector_3 Vector;
-	typedef SEACAVE::cList<Point, const Point&, 2, 8192> PointArr;
+	ofstream xmlWriter(XMLName, std::ios_base::binary | std::ios_base::out);
+	xmlWriter << setiosflags(ios::scientific)<< setprecision(16);
+	xmlWriter << "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" << endl
+		<< "<document version=\"0.9.1\">" << endl
+		<< "  <chunk>" << endl
+		<< "    <sensors>" << endl;
+	double zerof(0.0);
+	double onef(1.0);
 
-	std::vector<Point> points;
-
-	std::cout << scene.pointcloud.points.size() << " input points" << std::endl;
-	std::vector<std::size_t> indices(scene.pointcloud.points.size());
-	for (std::size_t i = 0; i < scene.pointcloud.points.size(); ++i) {
-		points.push_back(Point(scene.pointcloud.points[i].x, scene.pointcloud.points[i].y, scene.pointcloud.points[i].z));
-		indices[i] = i;
-	}
-	// simplification by clustering using erase-remove idiom
-	//const double removed_percentage = 90.0; // percentage of points to remove
-
-	std::vector<std::size_t>::iterator end;
-	end = CGAL::random_simplify_point_set(indices.begin(), indices.end(), &(scene.pointcloud.points[0]), removed_percentage);
-
-
-	std::size_t k = end - indices.begin();
-
-	std::cerr << "Keep " << k << " of " << indices.size() << "indices" << std::endl;
-	std::vector<int> indexVec;
-	std::set<int> indexSet;
-	std::ofstream indexWriter("indexFile.txt");
+	for (int i = 0; i < cameraVec.size(); i++)
 	{
-		for (std::size_t i = 0; i < k; ++i) {
-			indexVec.push_back(indices[i]);
-			indexSet.insert(indices[i]);
-		}
+		xmlWriter << "      <sensor id=\"" << i << "\" label=\"unknown\" type=\"frame\">" << endl
+			<< "        <resolution width=\"8176\" height=\"6132\"/>" << endl
+			<< "        <property name=\"fixed\" value=\"false\"/>" << endl
+			<< "        <calibration type=\"frame\" class=\"adjusted\">" << endl
+			<< "          <resolution width=\"8176\" height=\"6132\"/>" << endl
+			<< "          <fx>" << cameraVec.at(i).K(0, 0) << "</fx>" << endl
+			<< "          <fy>" << cameraVec.at(i).K(1, 1) << "</fy>" << endl
+			<< "          <cx>" << cameraVec.at(i).K(0, 2) << "</cx>" << endl
+			<< "          <cy>" << cameraVec.at(i).K(1, 2) << "</cy>" << endl
+			<< "          <k1>" << zerof << "</k1>" << endl
+			<< "          <k2>" << zerof << "</k2>" << endl
+			<< "          <k3>" << zerof << "</k3>" << endl
+			<< "        </calibration>" << endl
+			<< "      </sensor>"<<endl;
 	}
-
-
-
-
-	//vector<ANNkd_tree*> kdTreeVec;
-	//construct_ann_tree(scene, "F:\\GitHub\\openMVS_sample\\undistorted_images_samll\\out", kdTreeVec);
-	vector<cv::Mat> lineMapVec;
-	construct_line_map(scene, imagePath, lineMapVec, lineWidth);
-	for (int pointIndex = 0; pointIndex < scene.pointcloud.points.size(); pointIndex++)
+	xmlWriter << "    </sensors>" << endl 
+		<< "    <cameras>" << endl;
+	for (int i = 0; i < cameraVec.size(); i++)
 	{
-		if (pointIndex % 10000 == 0)
-		{
-			cout << pointIndex << endl;
-		}
-		TPoint3<float> pointMVS = scene.pointcloud.points[pointIndex];
-		Vec4d point3d;
-		point3d[0] = pointMVS.x;
-		point3d[1] = pointMVS.y;
-		point3d[2] = pointMVS.z;
-		point3d[3] = 1;
-
-		bool pointNearLine(false);
-		for (int viewIndex = 0; viewIndex < scene.pointcloud.pointViews[pointIndex].size(); viewIndex++)
-		{
-			int imageIndex = scene.pointcloud.pointViews[pointIndex][viewIndex];
-			Vec3d point2d = pMatVec.at(imageIndex) * point3d;
-			double x = point2d[0] / point2d[2];
-			double y = point2d[1] / point2d[2];
-			if (find_in_map(lineMapVec.at(imageIndex), point2d[0] / point2d[2], point2d[1] / point2d[2]))
-			{
-				pointNearLine = true;
-			}
-
-		}
-		if (pointNearLine)
-		{
-			indexVec.push_back(pointIndex);
-			indexSet.insert(pointIndex);
-		}
-
+		xmlWriter << "      <camera id=\"" << i << "\" label=\"" << imageNameVec.at(i) << "\" sensor_id=\"" << i << "\" enabled=\"true\"> " << endl
+					<< "        <resolution width=\"8176\" height=\"6132\"/> " << endl
+					<< "        <transform>" << cameraVec.at(i).R(0, 0) << " " << cameraVec.at(i).R(1, 0) << " " << cameraVec.at(i).R(2, 0) << " " << cameraVec.at(i).C.x -533500.0 << " "
+					<< cameraVec.at(i).R(0, 1) << " " << cameraVec.at(i).R(1, 1) << " " << cameraVec.at(i).R(2, 1) << " " << cameraVec.at(i).C.y -337960.0<< " "
+					<< cameraVec.at(i).R(0, 2) << " " << cameraVec.at(i).R(1, 2) << " " << cameraVec.at(i).R(2, 2) << " " << cameraVec.at(i).C.z << " "
+					<< zerof << " " << zerof << " " << zerof << " " << onef << "</transform>" << endl
+					<< "      </camera>" << endl;
 	}
 
-	std::sort(indexVec.begin(), indexVec.end());
-	for (int i = 0; i < indexVec.size(); i++)
-	{
-		indexWriter << indexVec.at(i) << std::endl;
-	}
-	indexWriter.close();
-
-	const int pointSize = scene.pointcloud.GetSize();
-	for (int i = pointSize - 1; i > -1; i--)
-	{
-		if (indexSet.find(i) == indexSet.end())
-		{
-			scene.pointcloud.RemovePoint(i);
-		}
-	}
-
-	scene.pointcloud.Save("simplicated.ply");
-	scene.Save("simplicated.mvs");
+	xmlWriter << "    </cameras>"<<endl
+		<<"  </chunk>"<<endl
+		<<"</document>";
+	xmlWriter.close();
 	return true;
 }
 
 int main(int argc, char** argv)
 {
-
-	typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
-	typedef Kernel::Point_3 Point;
-	typedef Kernel::Vector_3 Vector;
-	typedef SEACAVE::cList<Point, const Point&, 2, 8192> PointArr;
-
-	Scene sceneOri;
-	//sceneOri.Load("F:\\GitHub\\openMVS_sample\\undistorted_images\\sence2.mvs");
-	sceneOri.Load(argv[1]);
-
-
+	//string pMatPath = "D:\\Projects\\mapping_angle_area\\data\\wdob_lib_pmatrix";
+	string pMatPath = "data";
 	vector<Matrix3x4d> pMatVec;
-	for (int i = 0; i < sceneOri.images.size(); i++)
+	string search_path = pMatPath + "\\*.txt";
+	vector<string> pMatNameVec;
+	if (!get_filelist_from_dir(search_path, pMatNameVec))
 	{
-		Camera camera = sceneOri.images[i].camera;
-		camera.ComposeP();
-		cout << camera.P << endl;
-		pMatVec.push_back(camera.P);
+		cout << "open Projection Matrix failed!" << endl;
+		return false;
+	}
+	vector<string> imageNameVec;
+	for (int i = 0; i < pMatNameVec.size(); i++)
+	{		
+		string imageName = pMatNameVec.at(i).substr(0, pMatNameVec.at(i).find_last_of('.')) + ".jpg";
+		imageNameVec.push_back(imageName);
+	}
+
+	for (int i = 0; i < pMatNameVec.size(); i++)
+	{
+		pMatNameVec.at(i) = pMatPath +"\\"+ pMatNameVec.at(i);
+	}
+
+	readInPmatFile(pMatNameVec, pMatVec);
+	vector<Camera> cameraVec;
+	for (int i = 0; i < pMatNameVec.size(); i++)
+	{
+		Camera camera(pMatVec.at(i));
+		camera.DecomposeP();
+		cameraVec.push_back(camera);
 	}
 
 
-	Scene scene;
-	scene.Load(argv[2]);
-	std::cout << scene.platforms.begin()->cameras.size();
-
-	pointcloud_simplicate(scene, pMatVec,argv[3], atof(argv[4]),atoi(argv[5]));
-
-	//sence.pointcloud.pointViews[10][0];
-	//Camera camera = scene.images[0].camera;
+	writeXML(cameraVec, "cam.xml", imageNameVec);
 	//string imageName = scene.images[0].name;
 	//string platForm = scene.platforms[0].name;
 
@@ -282,6 +178,10 @@ int main(int argc, char** argv)
 	//std::cout << "C mat: " << std::endl << camera.C << std::endl;
 	//std::cout << "R mat: " << std::endl << camera.R << std::endl;
 
+	//Camera camera2(camera.K, camera.R, camera.C);
+	//camera2.ComposeP();
+	//cout << "P Mat: " << endl << camera2.P << endl;
+	//getchar();
 	//camera.ComposeP();
 	//std::cout << camera.P << std::endl;
 
